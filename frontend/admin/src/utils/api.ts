ea27dd5c565e8e30153856ad64a172b6e5f023c5
Lawrence <tsungulawrence@gmail.com>
2026-03-19 20:21:33 +0300

    code

--- frontend/admin/src/utils/api.ts
+++ frontend/admin/src/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5000/api/v1',
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://0f7b-102-212-236-165.ngrok-free.app/api/v1',
+  // baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
      'ngrok-skip-browser-warning': 'true'
