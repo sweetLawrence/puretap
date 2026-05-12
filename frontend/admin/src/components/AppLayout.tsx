c334f97f4656d4fe5ab09c8848942b765a9c07e5
Lawrence <tsungulawrence@gmail.com>
2026-05-12 11:55:49 +0300

    feedback added

--- frontend/admin/src/components/AppLayout.tsx
+++ frontend/admin/src/components/AppLayout.tsx
@@ -262,7 +262,7 @@ const navItems: NavItem[] = [
   //   )
   // },
   {
-    label: 'Customer Status',
+    label: 'Payment Status',
     path: '/customer-status',
     roles: ['admin','field_staff'],
     icon: (
@@ -277,6 +277,38 @@ const navItems: NavItem[] = [
         <path d='M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z' />
         <polyline points='14 2 14 8 20 8' />
       </svg>
+    )
+  },
+
+   {
+    label: 'Customer Feedback',
+    path: '/admin-feedback',
+    roles: ['admin','field_staff'],
+    icon: (
+      // <svg
+      //   width='18'
+      //   height='18'
+      //   viewBox='0 0 24 24'
+      //   fill='none'
+      //   stroke='currentColor'
+      //   strokeWidth='2'
+      // >
+      //   <path d='M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z' />
+      //   <polyline points='14 2 14 8 20 8' />
+      // </svg>
+
+        <svg
+    width="18"
+    height="18"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+  >
+    <path d="M21 15a4 4 0 0 1-4 4H8l-5 3V7a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4z" />
+    <line x1="8" y1="9" x2="16" y2="9" />
+    <line x1="8" y1="13" x2="14" y2="13" />
+  </svg>
     )
   }
 ]
