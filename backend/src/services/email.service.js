a3f8982e57fbe986e97bcbc7a8a714727e00575b
Lawrence <tsungulawrence@gmail.com>
2026-06-08 22:23:28 +0300

    code

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,32 +1,5 @@
-// import transporter from '../config/mailer.js'
-// import sendMail from '../config/mailer.js'
-
-// export const sendEmail = async (to, subject, html) => {
-//   if (!to) throw new Error('No email address provided')
-
-//   console.log('Sending email to:', to)
-//   console.log('Subject:', subject)
-
-//   const info = await transporter.sendMail({
-//     from: process.env.MAIL_FROM,
-//     to,
-//     subject,
-//     html
-//   })
-
-//   console.log('Email sent:', info.messageId)
-//   console.log('Response:', info.response)
-//   return info
-// }
-
-
-
-
-
-
-
-
-import sendMail from '../config/mailerxx.js'
+import transporter from '../config/mailer.js'
+import sendMail from '../config/mailer.js'
 
 export const sendEmail = async (to, subject, html) => {
   if (!to) throw new Error('No email address provided')
@@ -34,8 +7,35 @@ export const sendEmail = async (to, subject, html) => {
   console.log('Sending email to:', to)
   console.log('Subject:', subject)
 
-  const info = await sendMail({ to, subject, html })
+  const info = await transporter.sendMail({
+    from: process.env.MAIL_FROM,
+    to,
+    subject,
+    html
+  })
 
-  console.log('Email sent:', info?.id)
+  console.log('Email sent:', info.messageId)
+  console.log('Response:', info.response)
   return info
 }
+
+
+
+
+
+
+
+
+// import sendMail from '../config/mailerxx.js'
+
+// export const sendEmail = async (to, subject, html) => {
+//   if (!to) throw new Error('No email address provided')
+
+//   console.log('Sending email to:', to)
+//   console.log('Subject:', subject)
+
+//   const info = await sendMail({ to, subject, html })
+
+//   console.log('Email sent:', info?.id)
+//   return info
+// }
